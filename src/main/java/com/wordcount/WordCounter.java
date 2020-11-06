0fc05095f2cd69a0981e0cb9e0ff66cd7a0e1fcc
Paulius Olsevskas <paulius.olsevskas@erstegroup.com>
2020-11-06 18:40:29 +0100

    Change regex and add additional tests

--- src/main/java/com/wordcount/WordCounter.java
+++ src/main/java/com/wordcount/WordCounter.java
@@ -1,15 +1,29 @@
 package com.wordcount;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WordCounter {
 
-    public int count(String input) {
-//        TODO add guard clauses?
-        return prepare(input);
+    private final Pattern pattern = Pattern.compile("(?<!\\S)[a-z]++(?=\\s|$|[^a-z\\d\\s]++(?!\\S))");
+
+    public int count(final String input) {
+        Objects.requireNonNull(input);
+        if (input.equals("")){
+            return 0;
+        }
+        final Matcher matcher = pattern.matcher(input);
+
+        return countOccurrences(matcher);
     }
 
-    private int prepare(String input) {
-//        TODO: replace with better counter
-        return input.split(" ").length;
+    private int countOccurrences(final Matcher matcher) {
+        int counter = 0;
+        while (matcher.find()) {
+            counter++;
+        }
+        return counter;
     }
 
 }
--- src/test/java/com/wordcount/WordCounterTest.java
+++ src/test/java/com/wordcount/WordCounterTest.java
@@ -1,16 +1,79 @@
 package com.wordcount;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WordCounterTest {
 
+    private final WordCounter sut = new WordCounter();
+
     @Test
-    void countsPlainWordsCorrectly() {
-        WordCounter sut = new WordCounter();
+    void countsSimpleWordsCorrectly() {
         final String input = "word word";
         int expected = 2;
+
+        int actual = sut.count(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void returnsZeroWhenInputIsEmpty() {
+        final String input = "";
+        int expected = 0;
+
+        int actual = sut.count(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void returnsZeroWhenInputIsWhitespace() {
+        final String input = " ";
+        int expected = 0;
+
+        int actual = sut.count(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void returnsZeroWhenInputIsCharacter() {
+        final String input = "*-+/=!@#$%^&*()_`~?][|\\";
+        int expected = 0;
+        Arrays.stream(input.split(""))
+                .forEach(symbol -> {
+            int actual = sut.count(input);
+            assertEquals(expected, actual);
+        });
+    }
+
+    @Test
+    void returnsZeroWhenInputStringDoesNotMeetDefinitionOfAWord() {
+        final String input = "w0rd";
+        int expected = 0;
+
+        int actual = sut.count(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void thowsNullpointerExceptionIfParameterIsNull() {
+        assertThrows(NullPointerException.class, () -> sut.count(null));
+    }
+
+    @ParameterizedTest
+    @CsvSource({".", "!", "?", ":", ";"})
+    void countsWordsEndingWithAPunctuationMark(final String punctuationMark) {
+        final String input = "word" + punctuationMark;
+        int expected = 1;
 
         int actual = sut.count(input);
 
