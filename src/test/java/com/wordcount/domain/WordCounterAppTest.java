f8b067b16cee229b1eaf08d0ba9fb0d257ce921c
Paulius Olsevskas <paulius.olsevskas@erstegroup.com>
2020-11-13 15:47:23 +0100

    add test

---
+++ src/test/java/com/wordcount/domain/WordCounterAppTest.java
@@ -0,0 +1,22 @@
+package com.wordcount.domain;
+
+import com.wordcount.io.console.Writer;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class WordCounterAppTest {
+
+    @Test
+    void delegatesWorkToCollaboratorsAndThoseWorkAsTheyShould() {
+        final String input = "test a t2st";
+        final Writer mockWriter = mock(Writer.class);
+        final StopWords mockStopWords = mock(StopWords.class);
+        when(mockStopWords.contain("a")).thenReturn(true);
+        final WordCounterApp sut = new WordCounterApp(input, mockWriter, mockStopWords);
+
+        sut.countWords();
+
+        verify(mockWriter, times(1)).write("Number of words: 1");
+    }
+}
