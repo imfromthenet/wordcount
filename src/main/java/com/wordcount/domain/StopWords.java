5bf86284bf5ec2451426bbfad23e8e95125d70bd
Paulius Olsevskas <paulius.olsevskas@erstegroup.com>
2020-12-16 09:45:30 +0100

    refactor: simplify StopWords by moving initialization to constructor and removing the predicate method

--- src/main/java/com/wordcount/domain/StopWords.java
+++ src/main/java/com/wordcount/domain/StopWords.java
@@ -1,33 +1,24 @@
 package com.wordcount.domain;
 
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 public class StopWords {
-    private String stopWordsAsString;
     private List<String> stopWords;
 
     public StopWords(String stopWordsAsString) {
-        this.stopWordsAsString = stopWordsAsString;
+        this.stopWords = toStopWords(stopWordsAsString);
     }
 
     public List<String> filter(List<String> words) {
-        if (stopWords == null) {
-            stopWords = prepareStopWords();
-        }
         return words.stream()
-                .filter(isNotStopWord())
+                .filter(word -> !stopWords.contains(word))
                 .collect(toList());
     }
 
-    private Predicate<String> isNotStopWord() {
-        return word -> !stopWords.contains(word);
-    }
-
-    private List<String> prepareStopWords() {
+    private List<String> toStopWords(String stopWordsAsString) {
         return Stream.of(stopWordsAsString.split(" ")).collect(toList());
     }
 }
