8f7f582280dd0cdc2bf8012c9e62fac88d514b83
Paulius Olsevskas <paulius.olsevskas@erstegroup.com>
2020-12-17 09:37:46 +0100

    refactor: rename param to better represent what it is

--- src/main/java/com/wordcount/io/UIFactory.java
+++ src/main/java/com/wordcount/io/UIFactory.java
@@ -4,9 +4,9 @@ import com.wordcount.domain.UI;
 
 public class UIFactory {
 
-    public static UI construct(String[] consoleParameter) {
-        if (consoleParameter.length == 1) {
-            return new UIImpl(new ConsoleUI(), new FileInputUI(consoleParameter[0]));
+    public static UI construct(String[] consoleParameters) {
+        if (consoleParameters.length == 1) {
+            return new UIImpl(new ConsoleUI(), new FileInputUI(consoleParameters[0]));
         }
         return new ConsoleUI();
     }
