20bc194ac599535ec05202d8256bd88ce23ec5a3
Paulius Olsevskas <paulius.olsevskas@erstegroup.com>
2020-12-17 10:16:30 +0100

    implementing index feature: change implementation of UIFactory to mend the failing tests. update assumptions

--- src/main/java/com/wordcount/io/UIFactory.java
+++ src/main/java/com/wordcount/io/UIFactory.java
@@ -2,12 +2,23 @@ package com.wordcount.io;
 
 import com.wordcount.domain.UI;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class UIFactory {
 
     public static UI construct(String[] consoleParameters) {
-        if (consoleParameters.length == 1) {
-            return new UIImpl(new ConsoleUI(), new FileInputUI(consoleParameters[0]));
+        Optional<String> firstNonFlagArg = getNonFlagArgumentFrom(consoleParameters);
+        if (firstNonFlagArg.isPresent()) {
+            return new UIImpl(new ConsoleUI(), new FileInputUI(firstNonFlagArg.get()));
         }
         return new ConsoleUI();
     }
+
+    private static Optional<String> getNonFlagArgumentFrom(String[] consoleParameters) {
+        return Arrays.stream(consoleParameters)
+                .filter(p -> !p.startsWith("-"))
+                .findFirst();
+    }
+
 }
